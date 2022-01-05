2d0725ef9031a5c2ac85c25cc043e8765a786fe8
PankornSupercool_PC <pankornsupercool@gmail.com>
2022-01-05 20:14:13 +0700

    g

---
+++ main/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SideBar from './components/SideBar'
+function App() {
+  return (
+    <div className="grid ml-5 mt-5">
+      <SideBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ main/src/components/SideBar.js
@@ -0,0 +1,36 @@
+const SideBar = () =>{
+    return  (
+<div class="grid space-y-4 ">
+    <div class="grid justify-center">
+    <div class=" grid w-96 h-72 bg-white shadow rounded pl-2 pr-2 justify-items-center">
+      w-96
+  </div>
+    </div>
+
+  <div class="grid w-80 bg-white shadow rounded ">
+      w-80
+  </div>
+  <div class="w-72 bg-white shadow rounded">
+      w-72
+  </div>
+  <div class="w-64 bg-white shadow rounded">
+      w-64
+  </div>
+  <div class="w-60 bg-white shadow rounded">
+      w-60
+  </div>
+  <div class="w-56 bg-white shadow rounded">
+      w-56
+  </div>
+  <div class="w-52 bg-white shadow rounded">
+      w-52
+  </div>
+  <div class="w-48 bg-white shadow rounded">
+      w-48
+  </div>
+</div>
+    );
+}
+
+
+export default SideBar;
