271846fd6ac2315a74e6b2d9b7110838df19e7c9
PankornSupercool_PC <pankornsupercool@gmail.com>
2022-01-09 02:02:49 +0700

    UI Prototype 1

--- main/src/App.js
+++ main/src/App.js
@@ -1,16 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+
 import Contents from './components/Contents'
 import NavBar from './components/NavBar'
+
+//<div class="min-h-screen flex flex-col">
+//<header class="p-4 bg-teal-500 text-center text-white font-bold">Header</header>
+//<main class="h-64 bg-teal-600 flex items-center flex-grow justify-center text-white">Main</main>
+//<footer class="p-4 bg-teal-500 text-center text-white font-bold">Footer</footer>
+//</div>
+
+
 function App() {
   return (
-    <div className="">
-      <div className="">
-        <div className="">
+    <div className="min-h-screen flex flex-col">
+      <div className="block relative h-20 p-6 bg-zinc-100 text-center text-blue-600 font-bold rounded shadow">
+        <div className="flex justify-start align-center content-center  ">
+        <h1 className="">Lizard</h1>
 
         </div>
+
       </div>
-      <NavBar/>
+      <div className="h-64 flex bg-purple-400 items-center flex-grow justify-center text-black">
+        <div className="bg-zinc-100 w-96 h-96 rounded-md shadow-2xl flex items-center justify-center">
+          <div className="">
+          <input className="flex mb-12 shadow appearance-none border rounded w-36 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="USDT"/>
+          <span className="justify-center items-center mb-12 flex bg-gray-700 
+          rounded w-24 ml-6 h-8 text-white hover:bg-gray-900  shadow">
+            Convert To
+          </span>
+          
+          <input className="flex shadow appearance-none border  rounded w-36 py-2 px-3 text-gray-700 mb-3 leading-tight 
+          focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="TERRA"/>
+          <button className="flex justify-center items-center  bg-purple-500 w-36 h-8 rounded mt-12 hover:bg-purple-600 text-white shadow">Connect Wallet</button>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
--- main/src/components/NavBar.js
+++ main/src/components/NavBar.js
@@ -3,9 +3,7 @@ import { VscGithubInverted } from "react-icons/vsc"
 
 const NavBar = () =>{
     return (
-      <div className="fixed top-0 left-0 h-screen w-48 m-0 justify-between
-      flex flex-col bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white">
-        <NavBarIcon icon={<VscGithubInverted size="28"/>} text={"Home"}/>
+      <div className="">
       </div>
     );
 }
